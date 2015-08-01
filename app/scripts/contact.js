80c104df4df986fb37870ed8830205d5c5b6e3d2
Nathan Beck <njosefbeck@gmail.com>
2015-08-01 14:41:17 -0500

    Added initial js files for necessary views

---
+++ app/scripts/contact.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Contact = React.createClass({
+	render: function() {
+		return (
+			<h2>
+				Contact
+			</h2> 
+		);
+	}
+});
---
+++ app/scripts/newclients.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Clients = React.createClass({
+	render: function() {
+		return (
+			<h2>
+				Clients
+			</h2> 
+		);
+	}
+});
